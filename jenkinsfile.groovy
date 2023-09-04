9d4e0bcb515e439bea60f990de76f49e43c63c13
Divya Ravindra <divya.ravindra@smartfoodsafe.com>
2023-09-04 22:21:50 +0530

    testing

---
+++ jenkinsfile.groovy
@@ -0,0 +1,22 @@
+pipeline{
+
+    agent any
+    environment {
+        DOCKERHUB_CREDENTIALS=credentials('dockerhub')
+    }
+    stages {
+      
+
+        stage('Build') {
+            steps {
+                sh 'echo hello world'
+'
+            }
+        }
+
+    
+    }
+
+    
+
+}
